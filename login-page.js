a5b1a8336585912b87cbfd336ca629dbfc31c7ef
Ethan James <64974312+TheBozzz34@users.noreply.github.com>
2020-12-21 11:31:53 -0700

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -10,7 +10,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "TheBozzz34" && password === "abc123") {
+    if (username === "user" && password === "web_dev") {
         // If the credentials are valid, show an alert box and reload the page
         alert("You have successfully logged in.");
         location.hehe.html();
